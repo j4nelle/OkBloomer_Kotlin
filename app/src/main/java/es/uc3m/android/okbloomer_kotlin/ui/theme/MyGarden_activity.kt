787ac101d7f3f0d41b562549c4d410d4db7c9072
j4nelle <janellepoussou@hotmail.fr>
2025-02-18 15:52:19 +0100

    creation of first two activities and  button work

--- app/src/main/java/es/uc3m/android/okbloomer_kotlin/ui/theme/MyGarden_activity.kt
+++ app/src/main/java/es/uc3m/android/okbloomer_kotlin/ui/theme/MyGarden_activity.kt
@@ -1,42 +1,39 @@
-package es.uc3m.android.okbloomer_kotlin
+package es.uc3m.android.okbloomer_kotlin.ui.theme
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import es.uc3m.android.okbloomer_kotlin.ui.theme.MyGarden_activity
-import es.uc3m.android.okbloomer_kotlin.ui.theme.OkBloomer_KotlinTheme
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import es.uc3m.android.okbloomer_kotlin.GreetingPreview
+import es.uc3m.android.okbloomer_kotlin.R
 
-class MainActivity : ComponentActivity() {
+class MyGarden_activity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-           OkBloomer_KotlinTheme {
-               GreetingPreview()
+            OkBloomer_KotlinTheme {
+                Gardenlist()
             }
         }
     }
 }
 
-
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview(modifier : Modifier = Modifier) {
+fun Gardenlist(modifier : Modifier = Modifier){
 
     val context = LocalContext.current
     // Create a launcher for starting the activity
@@ -50,25 +47,17 @@ fun GreetingPreview(modifier : Modifier = Modifier) {
     }
 
 
+
     Column {
-        Text(
-            text = "Greetings user !",
-            modifier = modifier
-        )
-        Button(onClick = { // Create an intent to start the activity
-            val intent = Intent(context, MyGarden_activity::class.java)
-            // Start the activity using the launcher
-            launcher.launch(intent)
-            context.startActivity (intent) })
+        // to see the plants of our garden in a column view
+        Button(onClick ={/*go to the activity displaying the plant infos, instance of the class plant*/})
         {
-            Text("Go to my garden")
+            Text("Plant 1")
         }
-
-        Button(
-            onClick = {})
+        Button(onClick = {/*go to the activity displaying the plant infos, instance of the class plant*/})
         {
-            Text("Add a new plant")
+            Text("Plant 2")
         }
-
+        /*We need to find a way to add as many buttons as possible to this activity */
     }
 }
