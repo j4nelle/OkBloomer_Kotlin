d74c79ec67d20150dcfbc1ed8e3f980e0dcd26af
j4nelle <janellepoussou@hotmail.fr>
2025-02-21 13:46:19 +0100

    still working on the git

--- app/src/main/java/es/uc3m/android/okbloomer_kotlin/Adding_Plant_question.kt
+++ app/src/main/java/es/uc3m/android/okbloomer_kotlin/Adding_Plant_question.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import es.uc3m.android.okbloomer_kotlin.ui.theme.MyGarden_activity
 import es.uc3m.android.okbloomer_kotlin.ui.theme.OkBloomer_KotlinTheme
 
 class Adding_Plant_question : ComponentActivity() {
