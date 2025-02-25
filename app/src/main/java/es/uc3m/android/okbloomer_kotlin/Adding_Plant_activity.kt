6eaefabb1ad27eb9d1c47cfb89a4a81f673ac8de
j4nelle <janellepoussou@hotmail.fr>
2025-02-25 14:23:49 +0100

    adding the activity to show the plant's attributes + the add plant button leads back to the gardenlist activity

--- app/src/main/java/es/uc3m/android/okbloomer_kotlin/Adding_Plant_activity.kt
+++ app/src/main/java/es/uc3m/android/okbloomer_kotlin/Adding_Plant_activity.kt
@@ -1,5 +1,6 @@
 package es.uc3m.android.okbloomer_kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -21,7 +25,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import es.uc3m.android.okbloomer_kotlin.ui.theme.OkBloomer_KotlinTheme
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import es.uc3m.android.okbloomer_kotlin.ui.theme.MyGarden_activity
 
 class Adding_Plant_activity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,20 +47,27 @@ class Adding_Plant_activity : ComponentActivity() {
 @Composable
 fun AddingPreview_plant(modifier : Modifier = Modifier) {
     //function that gives the option of entering plant infos (should lead to the creation of an instance of Plant)
+
+    // Initiation of the variables
     var plant_name by rememberSaveable{ mutableStateOf("") }
     var specie by rememberSaveable { mutableStateOf("") }
     var watering_frequency by rememberSaveable { mutableStateOf("") }
 
+    //context variable
+    var context = LocalContext.current
 
     Column (
     modifier = Modifier.fillMaxSize(),
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally
     )
+
     {
+        Text(fontSize = 22.sp, text ="Describe your new plant :", color = Color.Blue)
         TextField(
             value = plant_name,
             onValueChange = { plant_name = it },
+            modifier = Modifier.fillMaxWidth(),
             placeholder = {
                 Text("Enter your plant's nickname")
             }
@@ -61,6 +76,7 @@ fun AddingPreview_plant(modifier : Modifier = Modifier) {
         TextField(
             value = specie,
             onValueChange = { specie = it },
+            modifier = Modifier.fillMaxWidth(),
             placeholder = {
                 Text("Enter your plant's specie")
             }
@@ -69,10 +85,23 @@ fun AddingPreview_plant(modifier : Modifier = Modifier) {
         TextField(
             value = watering_frequency,
             onValueChange = { watering_frequency = it },
+            modifier = Modifier.fillMaxWidth(),
             placeholder = {
                 Text("What is the watering frequency ?")
             }
         )
+
+        Button(
+            onClick ={
+                val intent = Intent(context, MyGarden_activity::class.java)
+                context.startActivity(intent)
+            }
+
+            //colors =
+        ) {
+        Text("Add to Garden")
+            // will have to switch to a text_button variable that says "added" when you click it
+        }
     }
 
 
